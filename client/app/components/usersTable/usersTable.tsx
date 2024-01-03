61d57693aa232f79cc749bd7e1513675190633fa
Quicc <kaganaslan56@gmail.com>
2024-01-04 02:27:14 +0300

    Checkbox functionality expanded in usersTable component and refactoring.

--- client/app/components/usersTable/usersTable.tsx
+++ client/app/components/usersTable/usersTable.tsx
@@ -1,10 +1,15 @@
 import styles from './usersTable.module.css';
+import { useEffect } from 'react';
 
 export default function UsersTable(props: any) {
-
   const users = props.users; // Imported from parent component, contains the list of users to be displayed.
   const selectedUsers = props.selectedUsers; // Imported from parent component, contains a lists for IDs of selected users.
-  const setSelecetedUsers = props.setSelecetedUsers;
+  const setSelecetedUsers = props.setSelecetedUsers; // Imported from parent component, used to trigger a data fetch.
+
+  useEffect(() => {
+    console.log('UsersTable component mounted.');
+    console.log(selectedUsers);
+  }, [selectedUsers]);
 
   /*
   Function Description:
@@ -17,9 +22,11 @@ export default function UsersTable(props: any) {
   const selectUser = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.checked) {
       // Add user ID to selectedUsers list.
-      const updatedSelectedUsers = Object.assign([], selectedUsers);
-      updatedSelectedUsers.push(e.target.value);
-      setSelecetedUsers(updatedSelectedUsers);
+      if (selectedUsers.indexOf(e.target.value) < 0) {
+        const updatedSelectedUsers = Object.assign([], selectedUsers);
+        updatedSelectedUsers.push(e.target.value);
+        setSelecetedUsers(updatedSelectedUsers);
+      }
     } else {
       // Remove user ID from selectedUsers list.
       if (selectedUsers.indexOf(e.target.value) > -1) {
@@ -30,12 +37,54 @@ export default function UsersTable(props: any) {
     }
   };
 
+  /*
+  Function Description:
+  Handler function for the checkbox input.
+  Adds or removes all user IDs to/from the selectedUsers list on checkbox change.
+  
+  Param:
+  e: React.ChangeEvent<HTMLInputElement>
+   */
+  const selectAllUsers = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedSelectedUsers = Object.assign([], selectedUsers);
+    if (e.target.checked) {
+      // Add all user IDs to selectedUsers list.
+      users.forEach((user: any) => {
+        const ID = String(user.ID);
+        if (updatedSelectedUsers.indexOf(ID) < 0) {
+          updatedSelectedUsers.push(ID);
+        }
+      });
+    } else {
+      // Remove all user IDs from selectedUsers list.
+      users.forEach((user: any) => {
+        const ID = String(user.ID);
+        if (updatedSelectedUsers.indexOf(ID) > -1) {
+          updatedSelectedUsers.splice(updatedSelectedUsers.indexOf(ID), 1);
+        }
+      });
+    }
+    setSelecetedUsers(updatedSelectedUsers);
+  };
+
   return (
     <table className="table">
       {/* Table header. */}
       <thead>
         <tr>
-          <th></th>
+          <th>
+            <div className="form-check">
+              {
+                // If all users are selected, render a checked checkbox.
+                selectedUsers.length == users.length ? (
+                  <input className="form-check-input" type="checkbox" onChange={selectAllUsers} id="selectAllUsers" value="selectAllUsers" checked />
+                ) : (
+                  // Else, render an unchecked checkbox.
+                  <input className="form-check-input" type="checkbox" onChange={selectAllUsers} id="selectAllUsers" value="selectAllUsers" checked={false} />
+                )
+              }
+            </div>
+          </th>
           <th>Employee Name</th>
           <th>Title</th>
           <th>Email</th>
@@ -47,10 +96,18 @@ export default function UsersTable(props: any) {
       <tbody>
         {/* Map over users and display them in the table. */}
         {users.map((user: any) => (
-          <tr key={user.ID}>
+          <tr key={'UserRow' + String(user.ID)}>
             <td>
               <div className="form-check">
-                <input className="form-check-input" type="checkbox" value={user.ID} onChange={selectUser} id={user.ID} />
+                {
+                  // If the user is selected, render a checked checkbox.
+                  selectedUsers.indexOf(String(user.ID)) > -1 ? (
+                    <input className="form-check-input" type="checkbox" onChange={selectUser} id={String(user.ID)} value={String(user.ID)} checked />
+                  ) : (
+                    // Else, render an unchecked checkbox.
+                    <input className="form-check-input" type="checkbox" onChange={selectUser} id={String(user.ID)} value={String(user.ID)} checked={false} />
+                  )
+                }
               </div>
             </td>
             <td>
